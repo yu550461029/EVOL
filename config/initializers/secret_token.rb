79f8d17eb53bf42d7d81f867303b0a258634539b
yudagong <yu550461029@126.com>
2014-12-18 23:44:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EVOL::Application.config.secret_token = 'b2791b5684d53fae71be3050c0d197dc3fd74f120ace1d03efd14c25e3515a831e6965f63d7f67cd2790bec3e4e93732d40d0576cc9146ae3f63372d3f0a0c16'
